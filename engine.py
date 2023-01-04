0c29ebe6083b62da6ead6bc97dec575680798cde
Paul Fisher <paulozfisher@gmail.com>
2023-01-04 16:04:43 +0100

    added more functionality, the game works now, I just need to add a menu and an end state

--- engine.py
+++ engine.py
@@ -1,3 +1,5 @@
+
+
 class GameState:
     def __init__(self):
 
@@ -6,22 +8,53 @@ class GameState:
             ['4','5','6'],
             ['1','2','3']
         ]
-        self.human_to_move = True
+        self.x_to_move = True
         self.move_log = []
 
     def make_move(self,move):
-        self.board[move.square_row][move.square_col] = 'x'
-        self.move_log.append(move)
-        self.human_to_move = not self.human_to_move
+        if self.x_to_move:
+            if self.board[move.square_row][move.square_col].isdigit() and move not in self.move_log:
+                self.board[move.square_row][move.square_col] = 'x'
+                self.move_log.append(move)
+                self.x_to_move = not self.x_to_move
+        else:
+            if self.board[move.square_row][move.square_col].isdigit() and move not in self.move_log:
+                self.board[move.square_row][move.square_col] = 'o'
+                self.move_log.append(move)
+                self.x_to_move = not self.x_to_move
 
-    def get_valid_moves(self):
-        pass
+    def winner(self, symbol):
+        for row in self.board:
+            if all(square == symbol for square in row):
+                return True
+                        # Check columns
+        for col in range(3):
+            if all(self.board[row][col] == symbol for row in range(3)):
+                return True
+
+        # Check diagonals
+        if all(self.board[i][i] == symbol for i in range(3)):
+            return True
+        if all(self.board[i][2-i] == symbol for i in range(3)):
+            return True
+
+        return False
+        
+    def pause(self):
+        self.paused = True
+
+    def resume(self):
+        self.paused = False
 
 class Move:
-    
     def __init__(self,sq,board):
         self.square_row = sq[0]
         self.square_col = sq[1]
-        self.move_made = board[self.square_row][self.square_col]       
+        self.move_made = board[self.square_row][self.square_col] 
+
+class Player:
+    def __init__(self,player,score):
+        self.score = score
+        self.player = player    
 
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import engine
+from engine import GameState
 
 WIDTH = HEIGHT = 512
 DIMENSION = 3
@@ -7,28 +8,39 @@ SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 60
 IMAGES = {}
 
+
+x_score = engine.Player('x',0) # Player object X
+o_score = engine.Player('o',0) # Player object O
+
 def load_images():
     pieces = ['x','o']
     for piece in pieces:
-        IMAGES[piece] = pygame.transform.scale(pygame.image.load('./Assests/' + piece + '.png'),(SQ_SIZE,SQ_SIZE))
+        IMAGES[piece] = pygame.transform.scale(pygame.image.load('Tik-Tak-Toe-AI/Assests/' + piece + '.png'),(SQ_SIZE,SQ_SIZE))
 def main():
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
-    clock = pygame.time.Clock()
-    running = True
-    sq_selected = ()
-    player_clicks = []
-
-    gs = engine.GameState()
-    valid_moves = gs.get_valid_moves()
-    move_made = False
+    pygame.init()
     load_images()
 
+    ##############
+    # initialised variables
+    ##############
+    border_size = 50 # the size of the outer border
+    screen = pygame.display.set_mode((WIDTH + border_size * 2, HEIGHT + border_size * 2)) # the size of the total screen
+    clock = pygame.time.Clock() # initialise the clock
+    running = True # set the while loop to true
+    start_time = pygame.time.get_ticks() # get the initial time
+    counter = 20 # turn counter
+    sq_selected = () # which square will be selected
+    gs = engine.GameState() # initialise the GameState class
+    move = []
+    ##############
+    
 
     while running:
+
+
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
-                running == False
-
+                running = False
 
         #mouse stuff
             elif e.type == pygame.MOUSEBUTTONDOWN:
@@ -39,41 +51,114 @@ def main():
                     sq_selected = ()
                 else:
                     sq_selected = (row,col)
-                #if len(player_clicks) == 1:
                 move = engine.Move(sq_selected,gs.board)
-                    #if move in valid_moves:
                 gs.make_move(move)
-                move_made = True
+                
                 sq_selected = ()
-
-        if move_made:
-            move_made = False
-        draw_game_state(screen,gs)
+            
+        elapsed_time = (pygame.time.get_ticks() - start_time) / 1000
+        counter -= elapsed_time
+        start_time = pygame.time.get_ticks()
         clock.tick(MAX_FPS)
+
+
+
+        if gs.winner(x_score.player):
+            x_score.score += 1
+            draw_winner(screen,'X')
+            main()
+
+        if gs.winner(o_score.player):
+            o_score.score += 1
+            draw_winner(screen,'O')
+            main()
+
+        draw_game_state(screen,gs,border_size,counter,x_score,o_score)
+
+
         pygame.display.flip()
 
 
+def draw_game_state(screen,gs,border_size,counter,x,o):
+    draw_board(screen,border_size)
+    draw_pieces(screen,gs.board,border_size)
+    draw_clock(screen,counter)
+    draw_player_turn(screen,gs.x_to_move)
+    draw_score(screen,x,o)
+    
 
+def draw_board(screen,border_size):
+    # Clear the outer border area before drawing the new border
+    screen.fill((0, 0, 0), pygame.Rect(0, 0, WIDTH + border_size * 2, border_size))
+    screen.fill((0, 0, 0), pygame.Rect(0, HEIGHT + border_size, WIDTH + border_size * 2, border_size))
+    screen.fill((0, 0, 0), pygame.Rect(0, 0, border_size, HEIGHT + border_size * 2))
+    screen.fill((0, 0, 0), pygame.Rect(WIDTH + border_size, 0, border_size, HEIGHT + border_size * 2))
 
-
-
-def draw_game_state(screen,gs):
-    draw_board(screen)
-    draw_pieces(screen,gs.board)
-
-def draw_board(screen):
     colours = [pygame.Color('white'),pygame.Color('light blue')]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             colour = colours[((r+c)) % 2]
-            pygame.draw.rect(screen,colour,pygame.Rect(c*SQ_SIZE,r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+            pygame.draw.rect(screen, colour, pygame.Rect(c * SQ_SIZE + border_size, r * SQ_SIZE + border_size, SQ_SIZE, SQ_SIZE))
 
-def draw_pieces(screen,board):
+def draw_pieces(screen,board,border_size):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             piece = board[r][c]
-            if piece == 'x':
-                screen.blit(IMAGES[piece],pygame.Rect(c*SQ_SIZE,r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+            if piece in ['x','o']:
+                screen.blit(IMAGES[piece],pygame.Rect(c * SQ_SIZE + border_size, r * SQ_SIZE + border_size, SQ_SIZE, SQ_SIZE))
+
+def draw_clock(screen,counter):
+
+    font = pygame.font.SysFont(None, 75)
+    counter = round(counter)
+    text = font.render(str(counter), True, (0, 128, 0)) 
+    
+    text_rect = text.get_rect()
+    text_rect.top = screen.get_rect().top
+    text_rect.right = screen.get_rect().right
+    
+    screen.blit(text,text_rect)
+
+def draw_player_turn(screen,gs):
+    font = pygame.font.SysFont(None, 50)
+    
+    if gs == True:
+        text = font.render(str('Player X'), True, (0,128,0))
+        text_rect = text.get_rect()
+        text_rect.bottom = screen.get_rect().bottom
+        text_rect.left = screen.get_rect().left
+    else:
+        text = font.render(str('Player O'), True, (0,128,0))
+        text_rect = text.get_rect()
+        text_rect.topleft = screen.get_rect().topleft
+
+    screen.blit(text,text_rect)
+
+def draw_score(screen,x,o):
+    font = pygame.font.SysFont(None,50)
+    colour = 128
+    textx = font.render(str(x.score),True,(0,colour,0))
+    textx_rect = textx.get_rect()
+    texto = font.render(str(o.score),True,(0,colour,0))
+    texto_rect = texto.get_rect()
+    textx_rect.center = screen.get_rect().center
+    textx_rect.bottom = screen.get_rect().bottom
+    texto_rect.center = screen.get_rect().center
+    texto_rect.top = screen.get_rect().top
+
+    screen.blit(textx,textx_rect)
+    screen.blit(texto,texto_rect)
+
+
+def draw_winner(screen,winner):
+    font = pygame.font.SysFont(None, 50)
+    text = font.render(str(f'Player {winner} has won the game!'), True, (0,128,0))
+    text_rect = text.get_rect()
+    text_rect.center = screen.get_rect().center
+    screen.blit(text,text_rect)
+
+    
+
 
 if __name__ == "__main__":
     main()
